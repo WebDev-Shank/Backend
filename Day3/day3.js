24cec910cfa9d7263bf097c4f7284efee6130581
Shankar <shankarwebdev@gmail.com>
2025-05-07 22:16:13 +0530

    day 3

---
+++ Day3/day3.js
@@ -0,0 +1,22 @@
+// *3. Day3 [NPM Basics & Advanced Features]
+//!    a)Introduction
+//       npm -> node pacakage manager
+//       NPM is short for node package manager, an online directory that contains the various already registered open-source packages.
+
+//!    b)NPM Understanding
+//       NPM is a package manager used to install, delete, and update Javascript packages on your machine. NPX is a package executer, and it is used to execute javascript packages directly, without installing them.
+
+//!    c)Installing and uninstalling anything basics & advanced
+//       i)install -> npm i packageName or npm install packageName
+//       ii)ubstall -> npm uninstall packageName
+
+//!   d)Installing particular versions
+//       ->npm i packageName@1.0.1 [version]
+
+//!    e)Understanding Node modules
+//       In Node. js, Modules are the blocks of encapsulated code that communicate with an external application on the basis of their related functionality. Modules can be a single file or a collection of multiple files/folders.
+
+//!    f)Dependencies & Devdependencies
+//       A dependency is a library that a project needs to function effectively. DevDependencies are the packages a developer needs during development.
+
+//!    g)Scripts - understanding default scripts PATH and custom scripts
